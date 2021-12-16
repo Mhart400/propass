f9690bf14826587e7cbcac929f3e22de3f1c0374
Mhart400 <mhart400@gmail.com>
2021-12-16 11:58:32 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          COMING SOON
+          COMING SOON!
         </a>
       </header>
     </div>
