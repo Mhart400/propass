dd10395f3fe80256e2c9514c387548d189ec5799
Mhart400 <mhart400@gmail.com>
2021-12-19 16:34:41 -0500

    added user profile

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,22 +1,31 @@
-import React from 'react'
-import { Grid} from "@mui/material";
+import React from "react";
+import { Grid } from "@mui/material";
 import HeroSection from "../Components/HeroSection";
 import ropesImg from "../Images/ropes.jpeg";
+import { useAuth } from "../Context/AuthContext";
 
-const image = ropesImg
+const image = ropesImg;
 const HomeScreen = () => {
+  const { userProfile } = useAuth();
+
   return (
-    <Grid sx={{backgroundColor: 'black', minHeight: 'calc(100vh - 69px)'}} >
+    <Grid sx={{ backgroundColor: "black", minHeight: "calc(100vh - 69px)" }}>
       <HeroSection
-      height="700px"
-      imgSrc={image}
-      imgAlt="skdjfksdfksjd"
-      title="Hi Rich!!!"
-      subtitle="Connecting Pro's and Owners"
-      btnText="Sign-Up"
-      btnLink='/signup'
+        height="700px"
+        imgSrc={image}
+        imgAlt="skdjfksdfksjd"
+        title="Hi Rich!!!"
+        subtitle="Connecting Pro's and Owners"
+        btnText={userProfile ? "Continue to Home" : "Sign-up"}
+        btnLink={
+          userProfile["isOwner"] === true
+            ? "/owner/"
+            : userProfile["isPro"] === true
+            ? "/pro/"
+            : "/signup"
+        }
       />
-      </Grid>
+    </Grid>
   );
 };
 
